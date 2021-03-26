9f859e72fb75e87639e2e5f27a117932ffd49c1c
Tyler <tyler_venetsanos@msn.com>
2021-03-26 16:23:54 -0400

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendCat(Ralph) {
+    return cats.push('Ralph');
+}
+
+function destructivelyPrependCat(Bob) {
+    return cats.unshift('Bob');
+} 
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(Broom) {
+    return [...cats, Broom];
+}
+
+function prependCat(Arnold) {
+    return [Arnold, ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
